0c8abb168e5f1dc09c2fb6646b180a35f6e6c3e8
Isaac Praveen <icylisper@gmail.com>
2011-03-23 22:31:59 +0530

    fallback to no-rlwrap repl, if no rlwrap

--- modules/ns.sh
+++ modules/ns.sh
@@ -20,7 +20,12 @@ run() {
 }
 
 repl() {
-    rlwrap --break-chars "\"\\'(){}[],^%$#@;:|" --remember -c -f ${CLJR_BIN}/clj_completions $JARK _ns repl $* 
+    which rlwrap &> /dev/null
+    if [ $? == "0" ]; then
+        rlwrap --break-chars "\"\\'(){}[],^%$#@;:|" --remember -c -f ${CLJR_BIN}/clj_completions $JARK _ns repl $* 
+    else
+        $JARK _ns repl $* 
+    fi
 }
 
 load() {
