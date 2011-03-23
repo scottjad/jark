78deab05f5b8ca46db94dc05aedbeededdb23dc0
Isaac Praveen <icylisper@gmail.com>
2011-03-23 22:21:19 +0530

    Added color repl

--- modules/ns.sh
+++ modules/ns.sh
@@ -20,7 +20,7 @@ run() {
 }
 
 repl() {
-    rlwrap --remember -c -f ${CLJR_BIN}/clj_completions $JARK _ns repl $* 
+    rlwrap --break-chars "\"\\'(){}[],^%$#@;:|" --remember -c -f ${CLJR_BIN}/clj_completions $JARK _ns repl $* 
 }
 
 load() {
